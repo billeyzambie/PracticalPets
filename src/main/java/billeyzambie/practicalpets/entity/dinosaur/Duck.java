361a230a95104d71542a023be2fd0d331ba24dec
billeyzambie <billaddonzobi@outlook.com.gr>
2025-06-15 13:43:45 +0300

    Defined a nutrition for glistering melon for ducks

--- src/main/java/billeyzambie/practicalpets/entity/dinosaur/Duck.java
+++ src/main/java/billeyzambie/practicalpets/entity/dinosaur/Duck.java
@@ -2,7 +2,6 @@ package billeyzambie.practicalpets.entity.dinosaur;
 
 import billeyzambie.practicalpets.ModEntities;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.TamableAnimal;
@@ -10,6 +9,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.common.Tags;
+import org.jetbrains.annotations.NotNull;
 
 public class Duck extends AbstractDuck {
     public Duck(EntityType<? extends TamableAnimal> entityType, Level level) {
@@ -47,9 +47,11 @@ public class Duck extends AbstractDuck {
 
     @Override
     public HealOverride healOverride(ItemStack itemStack) {
-        //Seeds aren't edible so a healOverride must be defined
+        //Seeds and glistering melon aren't edible so a healOverride must be defined
         if (itemStack.is(Tags.Items.SEEDS))
             return new HealOverride(HealOverrideType.DEFINE_NUTRITION, 2);
+        if (itemStack.is(Items.GLISTERING_MELON_SLICE))
+            return new HealOverride(HealOverrideType.DEFINE_NUTRITION, 6);
         //Bread is bad for ducks
         if (itemStack.is(Items.BREAD))
             return new HealOverride(HealOverrideType.DEFINE_NUTRITION, 1);
@@ -72,7 +74,7 @@ public class Duck extends AbstractDuck {
     }
 
     @Override
-    public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob partner) {
+    public AgeableMob getBreedOffspring(@NotNull ServerLevel level, @NotNull AgeableMob partner) {
         Duck baby = ModEntities.DUCK.get().create(level);
         if (baby != null) {
             if (this.isTame()) {
