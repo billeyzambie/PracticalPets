7f7b6084e5a1141c89e742c29ac7d5e85627639c
billeyzambie <billaddonzobi@outlook.com.gr>
2026-03-09 10:16:54 +0200

    Made piranhas flip less often when flopping

--- src/main/java/billeyzambie/practicalpets/client/animation/fish/PiranhaAnimations.java
+++ src/main/java/billeyzambie/practicalpets/client/animation/fish/PiranhaAnimations.java
@@ -11,28 +11,28 @@ import net.minecraft.client.animation.KeyframeAnimations;
  * @author Author
  */
 public class PiranhaAnimations {
-	public static final AnimationDefinition flop = AnimationDefinition.Builder.withLength(2.6F).looping()
-		.addAnimation("body", new AnimationChannel(AnimationChannel.Targets.ROTATION,
-			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -90.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(1.16F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -90.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(1.28F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 90.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(2.48F,KeyframeAnimations.degreeVec(0.0F, 0.0F, 90.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(2.6F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 270.0F), AnimationChannel.Interpolations.LINEAR)
-		))
-		.addAnimation("body", new AnimationChannel(AnimationChannel.Targets.POSITION, 
-			new Keyframe(0.0F, KeyframeAnimations.posVec(-0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(1.16F, KeyframeAnimations.posVec(-0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(1.2F, KeyframeAnimations.posVec(-0.08F, 0.75F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(1.28F, KeyframeAnimations.posVec(0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(2.48F, KeyframeAnimations.posVec(0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(2.52F, KeyframeAnimations.posVec(0.08F, 0.75F, 0.0F), AnimationChannel.Interpolations.LINEAR),
-			new Keyframe(2.6F, KeyframeAnimations.posVec(-0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR)
-		))
-		.addAnimation("left_fin", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
-			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -37.5F), AnimationChannel.Interpolations.LINEAR)
-		))
-		.addAnimation("right_fin", new AnimationChannel(AnimationChannel.Targets.ROTATION, 
-			new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 37.5F), AnimationChannel.Interpolations.LINEAR)
-		))
-		.build();
+	public static final AnimationDefinition flop = AnimationDefinition.Builder.withLength(5.2F).looping()
+			.addAnimation("body", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+					new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -90.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(2.6F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -90.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(2.72F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 90.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(5.08F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 90.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(5.2F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 270.0F), AnimationChannel.Interpolations.LINEAR)
+			))
+			.addAnimation("body", new AnimationChannel(AnimationChannel.Targets.POSITION,
+					new Keyframe(0.0F, KeyframeAnimations.posVec(-0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(2.6F, KeyframeAnimations.posVec(-0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(2.64F, KeyframeAnimations.posVec(-0.08F, 0.75F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(2.72F, KeyframeAnimations.posVec(0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(5.08F, KeyframeAnimations.posVec(0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(5.12F, KeyframeAnimations.posVec(0.08F, 0.75F, 0.0F), AnimationChannel.Interpolations.LINEAR),
+					new Keyframe(5.2F, KeyframeAnimations.posVec(-0.25F, -0.25F, 0.0F), AnimationChannel.Interpolations.LINEAR)
+			))
+			.addAnimation("left_fin", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+					new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, -37.5F), AnimationChannel.Interpolations.LINEAR)
+			))
+			.addAnimation("right_fin", new AnimationChannel(AnimationChannel.Targets.ROTATION,
+					new Keyframe(0.0F, KeyframeAnimations.degreeVec(0.0F, 0.0F, 37.5F), AnimationChannel.Interpolations.LINEAR)
+			))
+			.build();
 }
