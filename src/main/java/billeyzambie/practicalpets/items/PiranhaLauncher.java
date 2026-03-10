7d5e1ade92155376b730df0e8b57486e67ed21d7
billeyzambie <billaddonzobi@outlook.com.gr>
2026-03-10 02:25:49 +0200

    Made the piranha launcher equippable on pets as a hat, still no functionality

--- src/main/java/billeyzambie/practicalpets/items/PiranhaLauncher.java
+++ src/main/java/billeyzambie/practicalpets/items/PiranhaLauncher.java
@@ -1,9 +1,31 @@
 package billeyzambie.practicalpets.items;
 
+import billeyzambie.practicalpets.entity.PracticalPet;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 
-public class PiranhaLauncher extends Item {
+public class PiranhaLauncher extends Item implements ItemModelPetCosmetic {
     public PiranhaLauncher() {
         super(new Properties().stacksTo(1));
+    }
+
+    @Override
+    public ScaleMode getScaleMode() {
+        return ScaleMode.NONE;
+    }
+
+    @Override
+    public Slot slot(ItemStack stack, PracticalPet pet) {
+        return Slot.HEAD;
+    }
+
+    @Override
+    public boolean canBePutOn(ItemStack stack, PracticalPet pet) {
+        return true;
+    }
+
+    @Override
+    public boolean causesBravery(ItemStack stack, PracticalPet pet) {
+        return false;
     }
 }
