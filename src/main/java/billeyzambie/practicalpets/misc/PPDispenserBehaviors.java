c87ccb3522d6c1aee7ccb5b41465dcce3df89a9c
billeyzambie <billaddonzobi@outlook.com.gr>
2026-03-15 21:28:38 +0200

    Piranha buckets can now be used by dispensers

--- src/main/java/billeyzambie/practicalpets/misc/PPDispenserBehaviors.java
+++ src/main/java/billeyzambie/practicalpets/misc/PPDispenserBehaviors.java
@@ -1,14 +1,20 @@
 package billeyzambie.practicalpets.misc;
 
 import billeyzambie.practicalpets.items.PiranhaLauncher;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockSource;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Position;
 import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.core.dispenser.DispenseItemBehavior;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.item.DispensibleContainerItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
@@ -42,12 +48,32 @@ public class PPDispenserBehaviors {
             }
 
             private float getUncertainty() {
-                return 6.0F;
+                return 0f;
             }
 
             private float getPower() {
-                return 1.1F;
+                return 1.1f;
             }
         });
+
+        DispenseItemBehavior bucketDispenseBehavior = new DefaultDispenseItemBehavior() {
+            private final DefaultDispenseItemBehavior defaultDispenseItemBehavior = new DefaultDispenseItemBehavior();
+
+            @Override
+            public ItemStack execute(BlockSource p_123561_, ItemStack p_123562_) {
+                DispensibleContainerItem dispensiblecontaineritem = (DispensibleContainerItem)p_123562_.getItem();
+                BlockPos blockpos = p_123561_.getPos().relative(p_123561_.getBlockState().getValue(DispenserBlock.FACING));
+                Level level = p_123561_.getLevel();
+                if (dispensiblecontaineritem.emptyContents((Player)null, level, blockpos, (BlockHitResult)null, p_123562_)) {
+                    dispensiblecontaineritem.checkExtraContent((Player)null, level, p_123562_, blockpos);
+                    return new ItemStack(Items.BUCKET);
+                } else {
+                    return this.defaultDispenseItemBehavior.dispense(p_123561_, p_123562_);
+                }
+            }
+        };
+
+        DispenserBlock.registerBehavior(PPItems.PIRANHA_BUCKET.get(), bucketDispenseBehavior);
+
     }
 }
