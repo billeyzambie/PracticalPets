fa329af256d13d4115c151ed65bd7d107335faff
billeyzambie <billaddonzobi@outlook.com.gr>
2026-03-26 10:17:37 +0200

    Crows now get the bowtie on tame and can use guarding mode and the pet menu

--- src/main/java/billeyzambie/practicalpets/mixin/alexsmobs/entity/CrowMixin.java
+++ src/main/java/billeyzambie/practicalpets/mixin/alexsmobs/entity/CrowMixin.java
@@ -2,13 +2,22 @@ package billeyzambie.practicalpets.mixin.alexsmobs.entity;
 
 import billeyzambie.practicalpets.entity.base.practicalpet.IPracticalPet;
 import com.github.alexthe666.alexsmobs.entity.EntityCrow;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.TamableAnimal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.UUID;
 
 @Mixin(EntityCrow.class)
 public abstract class CrowMixin extends TamableAnimal implements IPracticalPet {
@@ -78,6 +87,8 @@ public abstract class CrowMixin extends TamableAnimal implements IPracticalPet {
 
     @Shadow(remap = false) public abstract int getCommand();
 
+    @Shadow(remap = false) @Nullable private UUID seedThrowerID;
+
     @Override
     public FollowMode getFollowMode() {
         if (this.petIsCurrentlyGuarding())
@@ -105,6 +116,40 @@ public abstract class CrowMixin extends TamableAnimal implements IPracticalPet {
                 int command = (value.ordinal() + 1) % 3;
                 this.setCommand(command);
             }
+        }
+    }
+
+    @Inject(
+            remap = false,
+            method = "mobInteract",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/github/alexthe666/alexsmobs/entity/EntityCrow;setCommand(I)V"
+            ),
+            cancellable = true
+    )
+    private void onInteract(
+            Player player,
+            InteractionHand hand,
+            CallbackInfoReturnable<InteractionResult> cir
+    ) {
+        this.defaultPracticalPetMixinInteraction(player, cir);
+    }
+
+
+    @Inject(
+            remap = false,
+            method = "tick",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lcom/github/alexthe666/alexsmobs/entity/EntityCrow;setTame(Z)V"
+            ),
+            cancellable = true
+    )
+    private void onTame(CallbackInfo ci) {
+        if (this.seedThrowerID != null) {
+            Player player = this.level().getPlayerByUUID(this.seedThrowerID);
+            this.onPetEquipmentWearerFirstTame(player);
         }
     }
 }
